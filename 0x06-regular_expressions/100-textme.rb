c9552409e1fc12766fad16233d8edf53c06a4255
TechOgede <sammonius@live.com>
2023-05-06 11:44:58 -0700

    parse text logs

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(\[from:(?<from>\+?\d{11})\])\s(\[to:(?<to>\+?\d{11})\])\s(\[flags:(?<flags>-?\d:-?\d:-?\d:-?\d:-?\d)\])/).join(",")
+puts ARGV[0].scan(/from:(\+?\d{11})\]\s\[to:(\+?\d{11})\]\s\[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/).join(",")
