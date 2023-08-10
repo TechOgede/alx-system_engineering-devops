4ac431d5ac8b7da5f790871c05875d4abc403a5f
TechOgede <117809974+TechOgede@users.noreply.github.com>
2023-08-10 02:02:08 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 
 exec {'fix file extension':
     provider => shell,
-    command  => 'sudo sed -i "s/\.phpp/.php" /var/www/html/wp-settings.php'
+    command  => 'sudo sed -i "s/\.phpp/.php/" /var/www/html/wp-settings.php'
 }
