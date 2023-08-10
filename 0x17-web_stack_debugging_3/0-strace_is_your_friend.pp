229d69b9dd9fd81eb63cce01e5d22903ac8269e7
TechOgede <sammonius@live.com>
2023-08-10 00:55:48 +0000

    fixed typo in we-settings.php

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix errorneous line in Wordpress setting file to resolve the 500 error
+
+exec {'fix file extension':
+    provider => shell,
+    command  => 'sed -i "s/\.phpp/.php" /var/www/html/wp-settings.php'
+}
