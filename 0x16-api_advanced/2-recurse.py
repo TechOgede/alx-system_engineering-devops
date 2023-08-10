393905108631f56503638635f2b93d7e7c760dcc
TechOgede <sammonius@live.com>
2023-08-10 10:49:00 +0000

    recursively get list of titles in a subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' This module defines a function that
+employs recursion to returns a list of the top ten titles
+for the hot posts listed for a given subreddit
+'''
+
+
+def recurse(subbreddit, hot_list=[], after=None):
+    ''' Return list of titles of all hot articles oon a subreddit '''
+    import requests
+
+    url = f'https://reddit.com/r/{subbreddit}/hot.json'
+
+    if after:
+        url = f'https://reddit.com/r/{subbreddit}/hot.json?after={after}'
+
+    headers = {"User-Agent": "Alx/1.0"}
+    res = requests.get(url, headers=headers)
+    data = res.json()
+
+    if 'error' in data or not data.get('data').get('children'):
+        return None
+
+    for post in data.get('data').get('children'):
+        hot_list.append(post.get('data').get('title'))
+
+    next_page_marker = data.get('data').get('after')
+
+    if not next_page_marker:
+        return hot_list
+
+    return recurse(subbreddit, hot_list, next_page_marker)
